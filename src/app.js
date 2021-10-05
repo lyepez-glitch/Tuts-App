912b2c766c684721bd3d147e9a9791f7a7b751a4
Lucas Yepez <lucasyepez99@gmail.com>
2021-10-04 22:15:53 -0500

    added form

--- src/app.js
+++ src/app.js
@@ -9,8 +9,27 @@ class App extends React.Component {
     }
   }
   render() {
-    return <h1>Hello{this.state.name}</h1>
+    return (
+      <div>
+        <nav>
+        <h2>My App</h2>
+        <h3>Tutorials</h3>
+        <h3>Add</h3>
+      </nav>
+      <form>
+         <p>Title</p>
+      <input id = 'title' type = 'text' value = 'pick a title'></input>
+      <p>Description</p>
+      <input id = 'description' value='type something here'></input>
+      <br></br>
+      <input id = 'submit' type = "submit" ></input>
+      </form>
+      </div>
+
+
+    )
   }
+
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /.css$/,
+        use: ["style-loader","css-loader"]
+      },
+
+      {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
